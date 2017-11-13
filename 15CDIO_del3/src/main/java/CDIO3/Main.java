3aacefdff9726b7872026b8314ff0f75faf60ae2
alkj <s165477@student.dtu.dk>
2017-11-13 17:27:32 +0100

    her laver jeg bare et felt.

--- 15CDIO_del3/src/main/java/CDIO3/Main.java
+++ 15CDIO_del3/src/main/java/CDIO3/Main.java
@@ -1,6 +1,9 @@
 package CDIO3;
 
 import gui_main.GUI;
+
+import java.awt.Color;
+
 import gui_codebehind.GUI_BoardController;
 import gui_codebehind.GUI_FieldFactory;
 import gui_fields.GUI_Board;
@@ -8,6 +11,9 @@ import gui_fields.GUI_Chance;
 import gui_fields.GUI_Empty;
 import gui_fields.GUI_Field;
 import gui_fields.GUI_Street;
+import gui_fields.GUI_Player;
+import gui_fields.GUI_Start;
+import gui_main.*;
 
 
 
@@ -19,7 +25,17 @@ public class Main {
 		
 		GUI gui = new GUI();
 		
-		initializeGUI();
+		GUI_Field g = new GUI_Street();
+		g.setTitle("TITLE1");
+		g.setBackGroundColor(Color.blue);
+		GUI_Field[] f = new GUI_Field[1];
+		f[0] = g;
+		new GUI(f);
+		gui.showMessage("velkommen");
+		
+		
+		
+		//initializeGUI();
 		
 		
 	}
@@ -38,9 +54,10 @@ public class Main {
 			fields[i].setTitle("title1");
 		}
 		// Lav tomme felter som ikke skal bruges
-		for (int i = 25; i <= fields.length; i++) {
+		for (int i = 25; i < fields.length; i++) {
 			fields[i] = new GUI_Empty();
 		}
+		
 		
 		
 	
