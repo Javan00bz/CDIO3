bbe33e359d6c6cc4ae48220f103d0c2867c77f4c
Y0SSB0SS <josephine.mellin@gmail.com>
2017-11-13 14:57:23 +0100

    bla bla bla

--- 15CDIO_del3/src/main/java/main.java
+++ 15CDIO_del3/src/main/java/main.java
@@ -4,6 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("det her er den første linje");
 		System.out.println("her er den anden linje");
+		System.out.println("Dette er tredje linje ");
 	}
 
 }
