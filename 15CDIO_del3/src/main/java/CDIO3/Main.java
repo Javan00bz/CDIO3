453be50d9af35b8ca5a837d43259430e81ba08d6
alkj <s165477@student.dtu.dk>
2017-11-13 16:24:49 +0100

    main klasse

---
+++ 15CDIO_del3/src/main/java/CDIO3/Main.java
@@ -0,0 +1,13 @@
+package CDIO3;
+
+import gui_main.GUI;
+
+public class Main {
+
+	public static void main(String[] args) {
+		GUI gui = new GUI();
+		gui.getUserButtonPressed("hej", "velkommen til monopoly æøå");
+
+	}
+
+}
