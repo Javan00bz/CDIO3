89cf123ba52f2965c404c9c8eb2f7c4f54c2f45e
alkj <s165477@student.dtu.dk>
2017-11-13 17:01:48 +0100

    guien er ændret, men jeg arbejder på det.

--- 15CDIO_del3/src/main/java/CDIO3/Main.java
+++ 15CDIO_del3/src/main/java/CDIO3/Main.java
@@ -1,13 +1,49 @@
 package CDIO3;
 
 import gui_main.GUI;
+import gui_codebehind.GUI_BoardController;
+import gui_codebehind.GUI_FieldFactory;
+import gui_fields.GUI_Board;
+import gui_fields.GUI_Chance;
+import gui_fields.GUI_Empty;
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Street;
+
+
+
+
 
 public class Main {
 
 	public static void main(String[] args) {
+		
 		GUI gui = new GUI();
-		gui.getUserButtonPressed("hej", "velkommen til monopoly æøå");
+		
+		initializeGUI();
+		
+		
+	}
 
+	
+	
+	
+	
+	private static void initializeGUI() {
+		
+
+		GUI_Field [] fields = new GUI_Field[40];
+		
+		for (int i = 0; i < 25; i++) {
+			fields[i] = new GUI_Street();
+			fields[i].setTitle("title1");
+		}
+		// Lav tomme felter som ikke skal bruges
+		for (int i = 25; i <= fields.length; i++) {
+			fields[i] = new GUI_Empty();
+		}
+		
+		
+	
 	}
 
 }
