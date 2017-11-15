a8edd02bf5863bd814dfd84ababf4ea0e8048ccc
oliveruschultz <oliveruldallschultz@gmail.com>
2017-11-15 16:37:08 +0100

    main

--- 15CDIO_del3/src/main/java/CDIO3/Main.java
+++ 15CDIO_del3/src/main/java/CDIO3/Main.java
@@ -14,41 +14,69 @@ import gui_fields.GUI_Street;
 import gui_fields.GUI_Player;
 import gui_fields.GUI_Start;
 import gui_main.*;
-
-
-
+import desktop_resources.*;
 
 
 public class Main {
 
+	private GUI gui = new GUI();
+	private Player Player1;
+	private Player Player2;
+	private Player Player3;
+	private Player Player4;
+	Player[] Players = {Player1, Player2, Player3, Player4};
+	private int startingMoney;
 	public static void main(String[] args) {
-		
-		GUI gui = new GUI();
-		
+
+		GUI_Player hej = new GUI_Player();
+		Main main = new Main();
 		GUI_Field g = new GUI_Street();
 		g.setTitle("TITLE1");
 		g.setBackGroundColor(Color.blue);
 		GUI_Field[] f = new GUI_Field[1];
 		f[0] = g;
 		new GUI(f);
-		gui.showMessage("velkommen");
+		main.startGame();
+
+
+
+
+
+
+		initializeGUI();
+
+
+	}
+	public void startGame() {
+		gui.showMessage("Welcome");
+		int amountOfPlayers = gui.getUserInteger("Hvor mange spillere er i?", 2, 4);
+		switch (amountOfPlayers) {
+		case 2: startingMoney = 20;
+		break;
+		case 3: startingMoney = 18;
+		break;
+		case 4: startingMoney = 16;
+		break;
+		default: startingMoney = 20;
+		}
 		
-		
-		
-		//initializeGUI();
-		
+		for(int i=1; i <= amountOfPlayers; i++) {
+			String playerName = gui.getUserString("Player " + i + " Enter your name" );
+			Players[i]  = new Player(playerName, 1, startingMoney);
+		}
+		for(int i=1; i <= amountOfPlayers; i++) {
+			GUI.addPlayer(Players[i].getName(), Players[i].getAccount().getValue());
+		}
 		
 	}
 
-	
-	
-	
-	
+
+
 	private static void initializeGUI() {
-		
+
 
 		GUI_Field [] fields = new GUI_Field[40];
-		
+
 		for (int i = 0; i < 25; i++) {
 			fields[i] = new GUI_Street();
 			fields[i].setTitle("title1");
@@ -56,11 +84,12 @@ public class Main {
 		// Lav tomme felter som ikke skal bruges
 		for (int i = 25; i < fields.length; i++) {
 			fields[i] = new GUI_Empty();
+
 		}
-		
-		
-		
-	
+
+
 	}
 
 }
+
+
