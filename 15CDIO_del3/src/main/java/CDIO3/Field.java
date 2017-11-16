9e2a31244f0873b3c95a1fb5677c859095cb85e5
Y0SSB0SS <josephine.mellin@gmail.com>
2017-11-16 19:16:24 +0100

    Field og GameBoard klasse, nothing big.

---
+++ 15CDIO_del3/src/main/java/CDIO3/Field.java
@@ -0,0 +1,19 @@
+package CDIO3;
+
+public class Field {
+	String nameoffield;
+
+	public Field(String nameoffield, int price, int tax) {
+		this.nameoffield = nameoffield;
+	}
+	
+	public String getNameoffield() {
+		return nameoffield;
+	}
+
+}
+
+
+
+
+
---
+++ 15CDIO_del3/src/main/java/CDIO3/GameBoard.java
@@ -0,0 +1,10 @@
+package CDIO3;
+
+import CDIO3.Field;
+public class GameBoard {
+
+	int [] Player = {1, 2, 3, 4};
+	Field[] fields = new Field[40];
+	
+}
+
